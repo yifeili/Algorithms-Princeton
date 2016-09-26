9f4289e06c1c63c05905ccc5ab451a3a99d49062
Yifei Li <syfifi1988@hotmail.com>
2016-09-25 21:54:40 -0500

    Add files via upload
    This programs are to estimate the value of the percolation threshold via Monte Carlo simulation.

---
+++ Assignment1_Percolation/Percolation.java
@@ -0,0 +1,104 @@
+
+public class Percolation {
+
+	private static boolean[][] opened;
+    private int index1 = 0;
+    private int index2;
+    private int size;
+    private WeightedQuickUnionUF qf;
+
+     /**
+      * Creates N-by-N grid, with all sites blocked.
+      * Initialize each site false.
+      */
+    public Percolation(int N) 
+    {
+        size = N;
+        index2 = size * size + 1;
+        qf = new WeightedQuickUnionUF(size * size + 2);
+        opened = new boolean[size][size];
+        for (int i=0; i<N; i++)
+        	for (int j=0; j<N; j++)
+        		opened[i][j] = false;
+    }
+
+     /**
+      * Opens site (row i, column j) if it is not open already.
+      */
+    public void open(int i, int j) 			// Use array qf as the monitor that whether the opened sites in "opened" are full.
+    {
+    	if (i < 1 || i > size || j < 1 || j > size)
+    		throw new IndexOutOfBoundsException();
+        
+    	opened[i-1][j-1] = true; 
+          
+        if (i == 1) {
+        	qf.union(getIndex(i, j), index1);
+        }
+        if (i == size) {
+            qf.union(getIndex(i, j), index2);
+        }
+
+        if (j > 1 && isOpen(i, j - 1)) {
+            qf.union(getIndex(i, j), getIndex(i, j - 1));
+        }
+        if (j < size && isOpen(i, j + 1)) {
+            qf.union(getIndex(i, j), getIndex(i, j + 1));
+        }
+        if (i > 1 && isOpen(i - 1, j)) {
+            qf.union(getIndex(i, j), getIndex(i - 1, j));
+        }
+        if (i < size && isOpen(i + 1, j)) {
+            qf.union(getIndex(i, j), getIndex(i + 1, j));
+        }
+    }
+
+          
+    /**
+     * Is site (row i, column j) open?
+     */
+    public boolean isOpen(int i, int j) 
+    {
+    	if (i < 1 || i > size || j < 1 || j > size)
+    		throw new IndexOutOfBoundsException();
+        return opened[i - 1][j - 1];
+    }
+
+    /**
+     * Is site (row i, column j) full?
+     */
+    public boolean isFull(int i, int j) 		// If site(row i, column j) in array of opened is full, in qf, the index of (row i, column j) should have the same root of index1.
+    {
+    	if (i < 1 || i > size || j < 1 || j > size)
+            throw new IndexOutOfBoundsException();
+        return qf.connected(index1, getIndex(i , j)); 
+    }
+
+    /**
+     * Does the system percolate?
+     */
+    public boolean percolates() {
+        return qf.connected(index1, index2);
+    }
+
+    /**
+     * Obtain the complete index of the 2D array.
+     */
+    private int getIndex(int i, int j) 
+    {
+         return size * (i - 1) + j;
+    }
+     
+    /**
+     * We can try to generate a 5*5 2D array.
+     */
+    public static void main(String[] args) {
+    	int N = 5;
+    	Percolation per = new Percolation(N);
+    	per.open(2,4);
+    	per.open(5,1);
+    	per.open(3,3);
+    	System.out.println(per.percolates());
+    }
+
+}
---
+++ Assignment1_Percolation/PercolationStats.java
@@ -0,0 +1,68 @@
+public class PercolationStats 
+{
+   private int Trytime;
+   private double[] Threshold;
+   private Percolation per;
+   
+   public PercolationStats(int N, int T)     // perform T independent experiments on an N-by-N grid
+   {
+       if (N <= 0 || T <= 0) 
+       {
+            throw new IllegalArgumentException("Given N <= 0 || T <= 0");
+       }
+       Trytime = T;
+       Threshold = new double[T]; 
+       for(int k = 0; k < T; k++) 
+       {
+           per = new Percolation(N);
+           int openedSites = 0;
+           while (!per.percolates()) // Keep opening sites (raw i, column j) until the array percolates. 
+           {
+        	   int i = StdRandom.uniform(1, N+1);
+        	   int j = StdRandom.uniform(1, N+1);
+        	   if (!per.isOpen(i,j))
+        	   {
+        		   per.open(i, j);
+        		   openedSites++;
+        	   }
+           }
+           double p = (double) openedSites/(N*N);
+           Threshold[k] = p;
+        }
+   }
+      
+   public double mean()                      // sample mean of percolation threshold
+   {
+       return StdStats.mean(Threshold);
+   }    
+       
+   public double stddev()                    // sample standard deviation of percolation threshold
+   {
+       return StdStats.stddev(Threshold);
+   }
+       
+       
+   public double confidenceLo()              // low  endpoint of 95% confidence interval
+   {
+       return mean() - (1.96 * stddev() / Math.sqrt(Trytime));
+   }
+       
+       
+   public double confidenceHi()              // high endpoint of 95% confidence interval
+   {
+       return mean() + (1.96 * stddev() / Math.sqrt(Trytime));
+   }
+
+   public static void main(String[] args)    // test client (described below)
+   {
+	   int N = Integer.parseInt(args[0]);
+       int T = Integer.parseInt(args[1]);
+       
+       PercolationStats Multiper = new PercolationStats(N,T);
+       
+       StdOut.println("mean = " + Multiper.mean());
+       StdOut.println("stddev = " + Multiper.stddev());
+       String confidence = Multiper.confidenceLo() + ", " + Multiper.confidenceHi();
+       StdOut.println("95% confidence interval = " + confidence);
+   }
+}
